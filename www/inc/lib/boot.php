55d4aa5032e8bd43b1d6e85f1ffa7b3e097f3277
soif <wxopwx@gmail.com>
2013-12-09 13:06:10 +0100

    Bump to version 0.131

--- www/inc/lib/boot.php
+++ www/inc/lib/boot.php
@@ -4,7 +4,7 @@ ini_set('display_errors','On');
 
 // base ------------------------------------------------------------------------------------
 $conf=array();
-$conf['app']['version']		="0.13";
+$conf['app']['version']		="0.131";
 $conf['app']['name']		="phpMyDomo";
 $conf['app']['url']			="http://www.phpmydomo.org";
 $conf['app']['protocol']	=(strtolower(substr($_SERVER["SERVER_PROTOCOL"],0,5))=='https') ? 'https://':'http://';
