9fe95e2ee5d7a3f8ccfb1fb7c9125099d9dbe454
soif <wxopwx@gmail.com>
2013-12-11 12:35:00 +0100

    check if .htaccess is there and allow subfolder

--- www/index.php
+++ www/index.php
@@ -1,3 +1,13 @@
 <?
-header("location: /home");
+$dir=dirname(__FILE__);
+if(!file_exists($dir.'/.htaccess')){
+	echo "
+	<h2>There is no .htaccess file in this diretory</h2>
+	Please copy it from the phpMyDomo/www/.htaccess archive to <i>$dir/.htaccess</i>:
+	<pre style='color:grey'>mv [ARCHIVE_PATH_OF_PMD]/phpMyDomo/www/.htaccess $dir/</pre>
+	";
+}
+$current_dir=dirname($_SERVER['PHP_SELF']);
+if($current_dir=='/'){$current_dir='';}
+header("location: $current_dir/home");
 ?>
