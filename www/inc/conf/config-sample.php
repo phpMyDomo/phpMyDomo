68f062f77d2c8ea5e15923ab180b2e89f95e73d9
soif <wxopwx@gmail.com>
2013-12-11 20:06:18 +0100

    OpenHab examples

--- www/inc/conf/config-sample.php
+++ www/inc/conf/config-sample.php
@@ -86,23 +86,36 @@ Example:
 	$conf['groups']['GROUP_KEY']['sensor'][]	="DEVICE_ID_3";
 will create a group named "GROUP_NAME" (with ID "GROUP_KEY") displaying switches for DEVICE_ID_1 and DEVICE_ID_2, and showing DEVICE_ID_3 sensor values in the group title.
 
-Another example:
+Another example: (working with openHab demo API)
+
 	// Groups Names ----------------------------------------------------
 	$conf['groups_names']['living']		="Living Room";
-	$conf['groups_names']['room1']		="John's BedRoom";
-	$conf['groups_names']['outside']	="OutSide";
+	$conf['groups_names']['outside']	="OutDoor";
+	$conf['groups_names']['heating']	="Heating";
 
 	// Groups (unique ids) ----------------------------------------------
-	$conf['groups']['living']['command'][]	="scene_group_2_status";
-	$conf['groups']['living']['command'][]	="command_switch_24_status";
-	$conf['groups']['living']['command'][]	="command_switch_25_status";
 
-	$conf['groups']['outside']['command'][]	="command_switch_27_status";
-	$conf['groups']['outside']['sensor'][]	="sensor_hum_7_humidity";
-	$conf['groups']['outside']['sensor'][]	="sensor_temp_7_temp";
+	$conf['groups']['outside']['command'][]	="command_switch_light_outdoor_terrace";
+	$conf['groups']['outside']['command'][]	="command_switch_light_outdoor_frontdoor";
+	$conf['groups']['outside']['command'][]	="command_switch_light_outdoor_garage";
+	$conf['groups']['outside']['sensor'][]	="sensor_temp_weather_temperature";
 
-	$conf['groups']['room1']['command'][]	="command_switch_26_status";
-	$conf['groups']['room1']['command'][]	="command_switch_42_status";
+	$conf['groups']['living']['command'][]	="scene_group_gf_living";
+	$conf['groups']['living']['command'][]	="command_switch_heating_gf_living";
+	$conf['groups']['living']['command'][]	="command_shutter_shutter_gf_living";
+	$conf['groups']['living']['command'][]	="command_dimmer_light_gf_living_table";
+	$conf['groups']['living']['sensor'][]	="sensor_bool_window_gf_living";
+	$conf['groups']['living']['sensor'][]	="sensor_temp_temperature_gf_living";
+
+	$conf['groups']['heating']['command'][]	="scene_group_heating";
+	$conf['groups']['heating']['command'][]	="command_switch_heating_gf_toilet";
+	$conf['groups']['heating']['command'][]	="command_switch_heating_gf_living";
+	$conf['groups']['heating']['command'][]	="command_switch_heating_gf_kitchen";
+	$conf['groups']['heating']['command'][]	="command_switch_heating_gf_corridor";
+	$conf['groups']['heating']['command'][]	="command_switch_heating_ff_office";
+	$conf['groups']['heating']['command'][]	="command_switch_heating_ff_child";
+	$conf['groups']['heating']['command'][]	="command_switch_heating_ff_bed";
+	$conf['groups']['heating']['command'][]	="command_switch_heating_ff_bath";
 
 */
 
@@ -122,28 +135,20 @@ Example:
 	$conf['blocks']['sensors'][]="DEVICE_ID_8";
 will show DEVICE_ID_4 and DEVICE_ID_5 in the 'weather' block, and DEVICE_ID_4 and DEVICE_ID_5 in the 'sensors' block
 
-Another Example:
+Another example: (working with openHab demo API)
+
 	// weather Block (unique ids) ---------------------------------------
-	$conf['blocks']['weather'][]="sensor_temp_7_temp";
-	$conf['blocks']['weather'][]="sensor_hum_7_humidity";
-	$conf['blocks']['weather'][]="sensor_baro_7_barometer";
-	$conf['blocks']['weather'][]="sensor_wind_temp_8_temp";
-	$conf['blocks']['weather'][]="sensor_wind_chill_8_chill";
-	$conf['blocks']['weather'][]="sensor_rain_10_rain";
-	$conf['blocks']['weather'][]="sensor_wind_speed_8_speed";
-	$conf['blocks']['weather'][]="sensor_wind_gust_8_gust";
+	$conf['blocks']['weather'][]="sensor_temp_weather_temperature";
 
-	// sensor Block (unique ids) ---------------------------------------
-	$conf['blocks']['sensors'][]="sensor_temp_28_temp";
-*/
-
-
-/*
-unset($conf['groups']);
-$conf['blocks']='';
-$conf['blocks']['weather'][]="sensor_wind_speed_rfxcom_ve_20_speed";
-$conf['blocks']['weather'][]="sensor_rain_rfxcom_pp_21";
-$conf['blocks']['sensors'][]="sensor_temp_rfxcom_tr1_19";
+	// Sensor Block (unique ids) ---------------------------------------
+	$conf['blocks']['sensors'][]="sensor_temp_temperature_setpoint";
+	$conf['blocks']['sensors'][]="sensor_temp_temperature_gf_living";
+	$conf['blocks']['sensors'][]="sensor_temp_temperature_gf_toilet";
+	$conf['blocks']['sensors'][]="sensor_temp_temperature_gf_corridor";
+	$conf['blocks']['sensors'][]="sensor_temp_temperature_ff_office";
+	$conf['blocks']['sensors'][]="sensor_temp_temperature_ff_child";
+	$conf['blocks']['sensors'][]="sensor_temp_temperature_ff_bed";
+	$conf['blocks']['sensors'][]="sensor_temp_temperature_ff_bath";
 */
 
 
