d771cf8f5d2e884c6148ae00324401a285e25611
Pepijn Goossens <pepijn@pepijn.com>
2013-12-13 14:01:12 +0100

    Added P1 Smart Meter support (Electricity)

--- www/inc/api/domoticz/api_client.php
+++ www/inc/api/domoticz/api_client.php
@@ -136,7 +136,24 @@ class PMD_ApiClient extends PMD_Root_ApiClient{
 					$d['unit']	="kWh";
 
 				}
+				elseif($raw['Type']=='P1 Smart Meter' AND $raw['SubType']=='Energy'){
+					$d['class']	='sensor';
+					
+					$d['type']	='current';
+					$d['value']	=(float) preg_replace('#[^0-9\.]+#','',$raw['CounterToday'])*1000;
+					$d['unit']	="kWh";
+					$this->RegisterDevice($d,'today');
 
+					$d['type']	='current';
+					$d['value']	=(float) preg_replace('#[^0-9\.]+#','',$raw['Usage']);
+					$d['unit']	="W";
+					$this->RegisterDevice($d,'now');
+
+					$d['type']	='current';
+					$d['value']	=(float) $raw['Counter'];
+					$d['unit']	="kWh";
+
+				}
 				elseif($raw['SwitchType']=='On/Off'){
 					$d['class']	='command';
 					$d['type']	='switch';
