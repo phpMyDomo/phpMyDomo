ebdcb675bb2704a9ee7582ff9c4797cf724495a1
Pepijn Goossens <pepijn@pepijn.com>
2013-12-13 10:10:11 +0100

    Dutch translation, remove typo

--- www/inc/lang/nl/global.php
+++ www/inc/lang/nl/global.php
@@ -4,7 +4,7 @@ $lang['locale']="nl_NL.utf8";
 
 // menu head ###################################################
 $lang['menu_head']['home']		="Dashboard";
-$lang['menu_head']['cameras']	="Cameras";
+$lang['menu_head']['cameras']	="Camera's";
 $lang['menu_head']['commands']	="Commando's";
 $lang['menu_head']['sensors']	="Sensoren";
 $lang['menu_head']['devices']	="Apparaten";
