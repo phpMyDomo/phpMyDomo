e20392d1c4c15ebf8dcb790ff9770f0b574c3445
soif <wxopwx@gmail.com>
2013-12-17 13:33:44 +0100

    Devices page Show a full Dump when adding ?dump to the url

--- www/inc/page/devices.php
+++ www/inc/page/devices.php
@@ -5,6 +5,9 @@ class PMD_Page extends PMD_Root_Page{
 	function Run(){
 		$data=array();
 		$data['rows']	=$this->o_api->GetDevices('','','type');
+		if(isset($_GET['dump'])){
+			$this->Debug('Devices Dump',$data['rows']);
+		}
 		$this->Assign('data',$data);
 		$this->Display();
 	}
