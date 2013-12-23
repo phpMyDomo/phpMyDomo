9ff14b442b9f72e06bdb05933a86758e5decc352
soif <wxopwx@gmail.com>
2013-12-23 09:48:32 +0100

    Fix PageError calls

--- www/inc/api/domoticz/api_client.php
+++ www/inc/api/domoticz/api_client.php
@@ -208,7 +208,7 @@ class PMD_ApiClient extends PMD_Root_ApiClient{
 		}
 		else{
 			if($this->debug){
-				$this->o_fn->PageError('500',"Failed to contact server at {$this->api_url} ");
+				$this->o_kernel->PageError('500',"Failed to contact server at {$this->api_url} ");
 			}
 		}
 		
@@ -226,7 +226,7 @@ class PMD_ApiClient extends PMD_Root_ApiClient{
 		}
 		else{
 			if($this->debug){
-				$this->o_fn->PageError('500',"Failed to contact server at {$this->api_url} ");
+				$this->o_kernel->PageError('500',"Failed to contact server at {$this->api_url} ");
 			}
 		}		
 	}
