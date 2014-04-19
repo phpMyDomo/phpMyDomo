c6e20a282272a688de98420640a6d3adb9f719e1
thefrip <lchauvin@laposte.net>
2014-04-19 08:39:36 +0200

    Enhancing Domogik api - Adding alarm device type

--- www/inc/api/domogik/api_client.php
+++ www/inc/api/domogik/api_client.php
@@ -34,35 +34,59 @@ class PMD_ApiClient extends PMD_Root_ApiClient{
 
 	//----------------------------------------------------------------------------------
 	function ApiListInfos(){
+
+	// todo: use wget -qO- http://www.gaisma.com/en/location/antibes.html | grep 'class="sunshine"' to get astronomy
+		
+		$this->infos['server_time'] = time();
 	}
 
 
 	//----------------------------------------------------------------------------------
 	function ApiListDevices(){
+
+		$time_start = microtime(true);
+
 		if($this->ApiFetch('list','device')){
 			$devices = $this->GetResult();
 			$i=0;
+			
 			foreach($devices as $d){
 				$raw=$d['raw'];
-				//address to send commands to
-				// Api Doc is not very usefull : What is the correct address?
-				//$d['address']=$raw['device_type']['device_technology_id'].'/'.$raw['address'];
-				$d['address']=$raw['device_type']['id'].'/'.$raw['address'];
-
+				$tech = explode(".", $raw['device_feature_model_id']);
+				$techno=$tech[0];
+				$device_feature=$tech[1];
+				$device_usage_id=$raw['device']['device_usage_id'];
+				
+				// device name
+				$d['name']=$raw['device']['name'];
+				
+				// address to send commands to
+				$d['address']=$techno.'/'.$raw['device']['address'];
+				
+				// first eliminate duplicates (dimmer declared as both change and switch)
+				if ($tech[2]=='change'){
+					continue;
+				}
+				
+				// eliminate elements without representation
+				if ($device_usage_id=='server' or $raw['device']['device_type_id']=='dawndusk.status'){
+					continue;
+				}
+				
 				// by master device usages
-				if($raw['device_usage_id']=='air_conditionning'){
+				if($device_usage_id=='air_conditionning'){
 					$d['class']	='command';
 					$d['type']	='therm';
 				}
-				elseif($raw['device_usage_id']=='ventilation'){
+				elseif($device_usage_id=='ventilation'){
 					$d['class']	='command';
 					$d['type']	='fan';
 				}
-				elseif($raw['device_usage_id']=='heating'){
+				elseif($device_usage_id=='heating'){
 					$d['class']	='command';
 					$d['type']	='heating';
 				}
-				elseif($raw['device_usage_id']=='shutter'){
+				elseif($device_usage_id=='shutter'){
 					$d['class']	='command';
 					$d['type']	='shutter';
 				}
@@ -168,51 +192,103 @@ class PMD_ApiClient extends PMD_Root_ApiClient{
 					$d['class']	='command';
 					$d['type']	='dimmer';
 				}
-
-				elseif($raw['device_type_id']=='dawndusk.status'){
-					continue;
+				
+				// by device type
+				
+				elseif($device_feature=='dimmer'){
+					$d['class']	='command';
+					$d['type']	='dimmer';
 				}
-
+				
+				elseif($device_feature=='switch'){
+					$d['class']	='command';
+					$d['type']	='switch';
+				}
+				
 				// by (fallback) device_usage_id ---------
 
-				elseif($raw['device_usage_id']=='appliance' or $raw['device_usage_id']=='light' or $raw['device_usage_id']=='christmas_tree' or $raw['device_usage_id']=='portal'){
+				elseif($device_usage_id=='light' or $device_usage_id=='christmas_tree'){
 					$d['class']	='command';
 					$d['type']	='switch';
 				}
 
-				elseif($raw['device_usage_id']=='water_tank'){
+				elseif($device_usage_id=='appliance'){
+					$d['class']	='command';
+					$d['type']	='alarm';
+				}
+
+				elseif($device_usage_id=='water_tank'){
 					$d['class']	='sensor';
 					$d['type']	='level';
 				}
-				elseif($raw['device_usage_id']=='temperature'){
+				elseif($device_usage_id=='temperature'){
 					$d['class']	='sensor';
 					$d['type']	='temp';
 				}
-				elseif($raw['device_usage_id']=='door'){
+				elseif($device_usage_id=='door' or $device_usage_id=='portal'){
 					$d['class']	='security';
 					$d['type']	='door';
 				}
-				elseif($raw['device_usage_id']=='window'){
+				elseif($device_usage_id=='window'){
 					$d['class']	='security';
 					$d['type']	='window';
 				}
 
-				elseif($raw['device_usage_id']=='security_camera'){
+				elseif($device_usage_id=='security_camera'){
 					$d['class']	='camera';
 					$d['type']	='cam_ip';
 				}
 
-				elseif($raw['device_usage_id']=='scene'){
+				elseif($device_usage_id=='scene'){
 					$d['class']	='scene';
 					$d['type']	='scene';
 				}
 				else{
 					
 				}
-				$this->RegisterDevice($d);
 
+				// fetch current value of the device
+				if (!isset($this->conf['api']['dbdatabase']) and $this->ApiFetch('list','stats', $d['raw']['device_id'] , $d['raw']['device_feature_model']['stat_key'])){
+
+					$stats=$this->api_response['stats'];
+					//if ($raw['device_id']=="45") $this->Debug('Stats',$stats);
+					
+					// case of boolean
+					if ($d['raw']['device_feature_model']['value_type']=="binary" or $d['raw']['device_feature_model']['value_type']=="boolean") {
+						
+						//compare with comparison json string
+						$stat_values=json_decode(html_entity_decode($d['raw']['device_feature_model']['parameters']), true);
+						$value = strtolower($stats[0]['value']);
+						
+						if ($value == strtolower($stat_values['value1']) || $value == 'preset_dim') {
+							$d['state'] = 'on';
+						} elseif ($value == strtolower($stat_values['value0']))
+							$d['state'] = 'off';
+							
+					} elseif ($d['raw']['device_feature_model']['value_type']=="number") {
+					
+						$d['value'] = (float) $stats[0]['value'];
+					}
+
+				} elseif($this->debug)
+					$this->o_kernel->PageError('500',"Failed to contact server at {$this->api_url} ");
+				
+				// register the full device
+				$this->RegisterDevice($d);
 			}
-			$this->_FetchValues();
+			
+			if (isset($this->conf['api']['dbdatabase']))
+				$this->_FetchValues();
+			
+			// additional device to measure query time
+			$time_end = microtime(true);
+			$time = $time_end - $time_start;
+			$tdev['name'] = 'Tx time';
+			$tdev['class'] = 'sensor';
+			$tdev['type'] = 'temp';
+			$tdev['value'] = (float) $time;
+			$this->RegisterDevice($tdev);
+
 			//$this->Debug('Devices',$this->devices);
 			//$this->Debug('JSON',$this->api_response);
 		}
@@ -223,37 +299,82 @@ class PMD_ApiClient extends PMD_Root_ApiClient{
 		}
 	}
 
-	// grab devices Values -------------------------------------------------------------
-	// not finished, need some work
-	
+	// grab device values
 	private function _FetchValues(){
-		if($this->ApiFetch('list','stats')){
-			$stats=$this->api_response['stats'];
-			foreach($this->devices as $uid => $d){
-				foreach($stats as $s){
-					if($d['raw']['id']==$s['device_id']){
-						$d['raw_stats']=$s;
-						//assign value
-						//value for commands should be ( 'on' | 'off' | 'int');
-						$d['value']=$s['value'];
-						if($d['value']=='none' or $d['value']=='0') $d['state']='off';
+		
+		// build multiple query
+		$query = "";
+		foreach($this->devices as $uid => $d){
+			if ($query != "") $query .= " union all ";
+			$query .= "(SELECT * FROM core_device_stats WHERE device_id = '" . $d['raw']['device_id'] . "' AND skey = '" . $d['raw']['device_feature_model']['stat_key'] . "' ORDER BY date DESC LIMIT 1)";
+		}
+		
+		// fetch data in db in one go
+		$mysqli = new mysqli("localhost", "domoread", "", "domogik");
+		if ($mysqli->connect_errno) {
+			$this->o_kernel->PageError('500', "Failed to connect to database : {$mysqli->connect_error} at {$this->api_url} ");
+		}
+		$res = $mysqli->query($query);
+		
+		// loop to read the results (assuming that both $this->devices and $rows are in the same order
+		// due to way data have been fetched in the db)
+		$row = $res->fetch_assoc();
+		foreach($this->devices as $uid => $d){
+		
+			//if ($d[raw]['device_id']=="45") $this->Debug('Row',$row);
+		
+			// check that result corresponds to current device (may happen that no result has been returned for a given device)
+			if ($row['device_id'] == $d['raw']['device_id']) {
+			
+				// case of boolean
+				if ($d['raw']['device_feature_model']['value_type']=="binary" or $d['raw']['device_feature_model']['value_type']=="boolean") {
+					
+					//compare with comparison json string
+					$stat_values=json_decode(html_entity_decode($d['raw']['device_feature_model']['parameters']), true);
+					$value = strtolower($row['value_str']);
+					
+					if ($value == strtolower($stat_values['value1']) || $value == 'preset_dim') {
+						$d['state'] = 'on';
+					} elseif ($value == strtolower($stat_values['value0']))
+						$d['state'] = 'off';
+				
+				// case of numeric value
+				} elseif ($d['raw']['device_feature_model']['value_type']=="number") {
+				
+					// take into account only if value not older than thirty minutes
+					if ((time() - $row['timestamp'])/60 < 30)
+						$d['value'] = (float) $row['value_num'];
+					
+					//$this->Debug('Row',time() . " " . $row['timestamp'] . " " . $row['date'] . " " . (time() - $row['timestamp'])/60);
+				}
+
+				// address specific case of dimmers
+				if ($d['type'] == 'dimmer') {
+				
+					$dres = $mysqli->query("SELECT * FROM core_device_stats WHERE device_id = '" . $d['raw']['device_id'] . "' AND skey = 'level' ORDER BY date DESC LIMIT 1");
+					
+					if ($row = $dres->fetch_assoc()) {
+					
+						$level = (float) $row['value_num'];;
+
+						// for unknown level domogik may return 255 !!!
+						if ($level >= 0 && $level <= 100) {
+							$d['value'] = $level;
+						}
+
+						// a 0 level indicates a device which is actually off
+						if ($level == 0) {
+							$d['state'] = 'off';
+						}
 					}
 				}
 				
-				//assign state to switches
-				if($d['class'] !='sensor' and $d['class'] !='camera'){
-					$d['state'] or $d['state']='on'; //just to test
-				}
-				// replace
 				$this->RegisterDevice($d);
+				
+				// read next row of result
+				$row = $res->fetch_assoc();
 			}
-			//$this->Debug('Stats',$stats);
 		}
-		else{
-			if($this->debug){
-				$this->o_kernel->PageError('500',"Failed to contact server at {$this->api_url} ");
-			}
-		}		
 	}
 } 
 ?>
--- www/inc/api/domogik/api_config.php
+++ www/inc/api/domogik/api_config.php
@@ -5,17 +5,20 @@
 
 $api['method']='json_get'; //api method : json_get | json_rpc2
 
-//api URL ----------------------------------------------------------------
+//api URL -------------------------------------------------------------------
 $api['urls']['api']	=$this->conf['urls']['host'].':40405';
 $api['urls']['www']	=$this->conf['urls']['host'].':40404';
 $api['dir_admin']	='/admin/';
 
+//api Direct database access - user needs only select privilege --------------
+$api['dbuser'] = 'domoread';
+$api['dbpassword'] = '';
+$api['dbdatabase'] = 'domogik';
 
 // Set Commands Values -------------------------------------------------------
 // Values used when Sending a command
-$api['set']['switch']['on']             ='on';
-$api['set']['switch']['off']            ='off';
-
+$api['set']['switch']['on']			='on';
+$api['set']['switch']['off']		='off';
 
 //$api['set']['scene']['on']		='On';
 //$api['set']['scene']['off']		='Off';
@@ -23,17 +26,22 @@ $api['set']['switch']['off']            ='off';
 //$api['set']['group']['on']		='On';
 //$api['set']['group']['off']		='Off';
 
-$api['set']['dimmer']['on']             ='on';
-$api['set']['dimmer']['off']            ='off';
-$api['set']['dimmer']['min']            =0;
-$api['set']['dimmer']['max']            =100;
+$api['set']['dimmer']['on']			='on';
+$api['set']['dimmer']['off']		='off';
+$api['set']['dimmer']['min']		=0;
+$api['set']['dimmer']['max']		=99;
+
+$api['set']['alarm']['on']			='arm';
+$api['set']['alarm']['off']			='disarm';
 
 // Actions -------------------------------------------------------------------
 $api['actions']['set']['switch']		='/command/{address}/{state}';
-//$api['actions']['set']['dimmer']		='/command/{address}/{state}';
-//$api['actions']['set']['dim_level']	='/command/{address}/{state}';
+$api['actions']['set']['dimmer']		='/command/{address}/{state}';
+$api['actions']['set']['dim_level']		='/command/{address}/dim/{state}';
 //$api['actions']['set']['scene']		='/command/{address}/{state}';
 //$api['actions']['set']['group']		='/command/{address}/{state}';
+
+$api['actions']['set']['alarm']		='/command/{address}/{state}';
 
 
 //#############################################################################
@@ -41,12 +49,8 @@ $api['actions']['set']['switch']		='/command/{address}/{state}';
 //#############################################################################
 
 // List actions URLS -------------------------------------
-$api['actions']['list']['device']		='/base/device/list';
-$api['actions']['list']['stats']		='/stats/*/*/latest';
-$api['actions']['set']['switch']                ='/command/{address}/{state}';
-$api['actions']['set']['dimmer']                ='/command/{address}/{state}';
-$api['actions']['set']['dim_level']             ='/command/{address}/dim/{state}';
-
+$api['actions']['list']['device']		='/base/feature/list';
+$api['actions']['list']['stats']		='/stats/{address}/{state}/latest';
 //$api['actions']['list']['scene']		='/';
 //$api['actions']['list']['camera']		='/';
 //$api['actions']['list']['info']		='/';
@@ -68,7 +72,7 @@ $api['fields']['name']				="name";
 
 //json_get definition ----------------------------------------------------------
 $api['json']['status']="status";	//field with the Status of the response
-$api['json']['result']="device";	//field with the list of devices
+$api['json']['result']="feature";	//field with the list of devices
 
 // json received statuts value in the "status" field
 $api['json_status']['ok']	='OK';
