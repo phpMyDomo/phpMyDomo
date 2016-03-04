e40a1c107fe212d93bfa8670b3756f356e7ebc29
soif <wxopwx@gmail.com>
2016-03-04 15:47:30 +0100

    Devices dump shows API request url

--- www/inc/page/devices.php
+++ www/inc/page/devices.php
@@ -6,7 +6,9 @@ class PMD_Page extends PMD_Root_Page{
 		$data=array();
 		$data['rows']	=$this->o_api->GetDevices('','',array('lang_class' => SORT_ASC, 'name' => SORT_ASC , 'lang_type' => SORT_ASC  ));
 		if(isset($_GET['dump'])){
-			$this->Debug('Devices Dump',$data['rows']);
+			$debug_arr['request_url']	=$this->o_api->api_url;
+			$debug_arr['devices']		=$data['rows'];
+			$this->Debug('Devices Dump',$debug_arr);
 		}
 		$this->Assign('data',$data);
 		$this->Display();
