b06958bbe620a1b4f8d08efb34826d4f33564757
soif <wxopwx@gmail.com>
2016-03-16 19:33:11 +0100

    bump version to 0.2a3 :star:

--- www/inc/lib/boot.php
+++ www/inc/lib/boot.php
@@ -4,7 +4,7 @@ ini_set('display_errors','On');
 
 // base ------------------------------------------------------------------------------------
 $conf=array();
-$conf['app']['version']		="0.2a2";
+$conf['app']['version']		="0.2a3";
 $conf['app']['name']		="phpMyDomo";
 
 //detect https
