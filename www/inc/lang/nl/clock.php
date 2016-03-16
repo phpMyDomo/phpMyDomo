17fa2ab421282d28e920b9fbda3b6ebfa5523022
soif <wxopwx@gmail.com>
2016-03-16 16:17:21 +0100

    Clock language files (base to translate into NL and DE) :beer:

---
+++ www/inc/lang/nl/clock.php
@@ -0,0 +1,23 @@
+<?php
+
+// main ###################################################
+$lang['main']['in']			="IN";		//to translate
+$lang['main']['out']		="OUT";		//to translate
+$lang['main']['but_alarm']	="Alarm";	//to translate
+$lang['main']['but_stop']	="Stop";	//to translate
+
+// modal alarm window ###################################################
+$lang['modal']['tab_timer']		="Timer";		//to translate
+$lang['modal']['tab_alarm']		="Alarm";		//to translate
+$lang['modal']['duration']		="Duration";	//to translate
+$lang['modal']['end_time']		="End Time";	//to translate
+$lang['modal']['sound']			="Ring Sound";	//to translate
+$lang['modal']['light']			="Visual Effect";	//to translate
+$lang['modal']['hours']			="hours";		//to translate
+$lang['modal']['minutes']		="minutes";		//to translate
+$lang['modal']['seconds']		="seconds";		//to translate
+$lang['modal']['but_cancel']	="Cancel";		//to translate
+$lang['modal']['but_timer']		="Set Timer";	//to translate
+$lang['modal']['but_alarm']		="Set Alarm";	//to translate
+
+?>
