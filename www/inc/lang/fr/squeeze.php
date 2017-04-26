9d2e1f03018a34e6c09c111f7ef130c284c2e300
soif <wxopwx@gmail.com>
2017-04-26 22:23:04 +0200

    Squeeze French language

---
+++ www/inc/lang/fr/squeeze.php
@@ -0,0 +1,76 @@
+<?php
+
+// Player buttons ###################################################
+
+$lang['search']			="Chercher";
+$lang['on']				="sur";
+
+$lang['prev']			="Morceau Précédent";
+$lang['next']			="Morceau Suivant";
+$lang['play']			="Play";
+$lang['pause']			="Pause";
+$lang['stop']			="Stop";
+$lang['rw']				="Rembobinage";
+$lang['ff']				="Avance Rapide";
+
+$lang['repeat_0']		="Pas de répétition";
+$lang['repeat_1']		="Répéter le morceau";
+$lang['repeat_2']		="Répéter la playlist";
+
+$lang['repeat_off']		="x";
+$lang['repeat_one']		="1";
+$lang['repeat_all']		="A";
+
+$lang['shuffle_0']		="Pas d'aléatoire";
+$lang['shuffle_1']		="Morceaux Aléatoires";
+$lang['shuffle_2']		="Albums Aléatoires";
+
+$lang['shuffle_off']	="x";
+$lang['shuffle_songs']	="S";
+$lang['shuffle_albums']	="A";
+//$lang['mute_on']		="Mute";
+//$lang['mute_off']		="UnMute";
+
+$lang['cue_in_set']		="Mémoriser le Point d'Entrée (IN)";
+$lang['cue_in_jump']	="Aller au Point d'Entrée (IN)";
+$lang['cue_out_set']	="Mémoriser le Point de Sortie (OUT)";
+$lang['cue_out_jump']	="Aller au Point de Sortie (OUT)";
+$lang['loop']			="Boucler entre les Points d'Entrée (IN) et de Sortie (OUT)";
+
+$lang['set_in']			="IN";
+$lang['set_out']		="OUT";
+
+
+$lang['volup']			="Volume +";
+$lang['voldown']		="Volume -";
+
+$lang['power']				="Power On/Off";
+$lang['restart_squeezelite']="Redémarrer SqueezeLite";
+$lang['mute']				="Mute On/Off";
+
+
+// Blocks ###################################################
+$lang['selected_player']="Lecteur Sélectionné";
+$lang['all_players']	="Controles Globaux";
+
+$lang['link_server']	="Serveur Local";
+$lang['link_ios']		="App. IOS";
+$lang['link_android']	="App. Android";
+
+
+$lang['g_mute_on']		="Muted";
+$lang['g_mute_off']		="Normal";
+
+$lang['g_power_on']		="On";
+$lang['g_power_off']	="Off";
+
+$lang['g_repeat_off']	="Non";
+$lang['g_repeat_one']	="Un";
+$lang['g_repeat_all']	="Tous";
+
+$lang['g_shuffle_off']		="Non";
+$lang['g_shuffle_songs']	="Art.";
+$lang['g_shuffle_albums']	="Alb.";
+
+
+?>
