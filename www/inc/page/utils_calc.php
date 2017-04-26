4a95d153d024997d5736579e8850b072d480224b
soif <wxopwx@gmail.com>
2017-04-26 14:35:09 +0200

    remove unused (device listing) code

--- www/inc/page/utils_calc.php
+++ www/inc/page/utils_calc.php
@@ -3,26 +3,6 @@ class PMD_Page extends PMD_Root_Page{
 
 	//----------------------------------------------------------------------------------
 	function Run(){
-/*
-		$data=array();
-		$api=$this->o_api->GetDevices('','',array('lang_class' => SORT_ASC, 'name' => SORT_ASC , 'lang_type' => SORT_ASC  ));
-		if(is_array($api)){
-			foreach($api as $name => $row){
-
-				if($row['type']=='switch' and $row['raw']['HardwareName']=="RFX usb"){
-					$data['rows'][$name]				=$row;
-					$data['rows'][$name]['house_code']	=chr($row['raw']['ID']);
-					
-				}
-			}
-			//$this->Debug('Devices Dump',$api);
-		}
-		//$data['rows']=$api;
-		if(isset($_GET['dump'])){
-			$this->Debug('Devices Dump',$data['rows']);
-		}
-		$this->Assign('data',$data);
-*/
 		$this->Display();
 	}
 
