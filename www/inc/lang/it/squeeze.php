9672869f30b7a6bc145af55598dd8d4de7da8dc8
soif <wxopwx@gmail.com>
2017-04-27 08:40:43 +0200

    Language file (to be translated)

---
+++ www/inc/lang/it/squeeze.php
@@ -0,0 +1,76 @@
+<?php
+
+// Player buttons ###################################################
+
+$lang['search']			="Search";
+$lang['on']				="on";
+
+$lang['prev']			="Previous Song";
+$lang['next']			="Next Song";
+$lang['play']			="Play";
+$lang['pause']			="Pause";
+$lang['stop']			="Stop";
+$lang['rw']				="Rewind";
+$lang['ff']				="Fast Forward";
+
+$lang['repeat_0']		="No Repeat";
+$lang['repeat_1']		="Repeat One";
+$lang['repeat_2']		="Repeat All";
+
+$lang['repeat_off']		="x";
+$lang['repeat_one']		="1";
+$lang['repeat_all']		="A";
+
+$lang['shuffle_0']		="No Shuffe";
+$lang['shuffle_1']		="Shuffle Songs";
+$lang['shuffle_2']		="Shuffle Albums";
+
+$lang['shuffle_off']	="x";
+$lang['shuffle_songs']	="S";
+$lang['shuffle_albums']	="A";
+//$lang['mute_on']		="Mute";
+//$lang['mute_off']		="UnMute";
+
+$lang['cue_in_set']		="Set IN Cue Point";
+$lang['cue_in_jump']	="Jump to IN Cue Point";
+$lang['cue_out_set']	="Set OUT Cue Point";
+$lang['cue_out_jump']	="Jump to OUT Cue Point";
+$lang['loop']			="Loop between IN and OUT points";
+
+$lang['set_in']			="IN";
+$lang['set_out']		="OUT";
+
+
+$lang['volup']			="Volume +";
+$lang['voldown']		="Volume -";
+
+$lang['power']			="Power On/Off";
+$lang['restart_squeezelite']="Restart SqueezeLite";
+$lang['mute']			="Mute On/Off";
+
+
+// Blocks ###################################################
+$lang['selected_player']="Selected Player";
+$lang['all_players']	="Global Controls";
+
+$lang['link_server']	="Local Server";
+$lang['link_ios']		="Launch IOS App.";
+$lang['link_android']	="Launch Android App.";
+
+
+$lang['g_mute_on']		="Muted";
+$lang['g_mute_off']		="Normal";
+
+$lang['g_power_on']		="On";
+$lang['g_power_off']	="Off";
+
+$lang['g_repeat_off']	="Off";
+$lang['g_repeat_one']	="One";
+$lang['g_repeat_all']	="All";
+
+$lang['g_shuffle_off']		="Off";
+$lang['g_shuffle_songs']	="Art.";
+$lang['g_shuffle_albums']	="Alb.";
+
+
+?>
