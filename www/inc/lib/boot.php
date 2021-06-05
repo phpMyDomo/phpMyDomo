e4cc25915eceda6b08692209f64bfceaa04d77fd
soif <wxopwx@gmail.com>
2021-06-05 19:10:56 +0200

    Version 21.6.5

--- www/inc/lib/boot.php
+++ www/inc/lib/boot.php
@@ -4,7 +4,7 @@ ini_set('display_errors','On');
 
 // base ------------------------------------------------------------------------------------
 $conf=array();
-$conf['app']['version']		="1.0.0b1";
+$conf['app']['version']		="21.6.5";
 $conf['app']['name']		="phpMyDomo";
 
 //detect https
