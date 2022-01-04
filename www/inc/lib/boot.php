3421cee0a49c323c077bf7e187605b054873d6ca
soif <wxopwx@gmail.com>
2022-01-04 16:22:45 +0100

    version 22.01040 (dev)

--- www/inc/lib/boot.php
+++ www/inc/lib/boot.php
@@ -4,7 +4,7 @@ ini_set('display_errors','On');
 
 // base ------------------------------------------------------------------------------------
 $conf=array();
-$conf['app']['version']		="21.1230";
+$conf['app']['version']		="22.01040";
 $conf['app']['name']		="phpMyDomo";
 
 //detect https
