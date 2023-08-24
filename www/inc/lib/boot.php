2d2851b54f028b4f8061446b847c65be21786761
soif <wxopwx@gmail.com>
2023-08-24 13:21:55 +0200

    fixes version

--- www/inc/lib/boot.php
+++ www/inc/lib/boot.php
@@ -4,7 +4,7 @@ ini_set('display_errors','On');
 
 // base ------------------------------------------------------------------------------------
 $conf=array();
-$conf['app']['version']		="23.08.24";
+$conf['app']['version']		="23.0824";
 $conf['app']['name']		="phpMyDomo";
 
 //detect https
