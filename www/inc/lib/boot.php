7e48606f2395aa81257427dbe044905317dbc63b
soif <wxopwx@gmail.com>
2023-08-24 13:00:29 +0200

    Version 23.08240 (dev)

--- www/inc/lib/boot.php
+++ www/inc/lib/boot.php
@@ -4,7 +4,7 @@ ini_set('display_errors','On');
 
 // base ------------------------------------------------------------------------------------
 $conf=array();
-$conf['app']['version']		="23.08050";
+$conf['app']['version']		="23.08240";
 $conf['app']['name']		="phpMyDomo";
 
 //detect https
